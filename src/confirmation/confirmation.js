90e0ea2b4724b3a049cb3ddb7b8aaf223443ea7f
nathan bonnet <nathanbonnet@macbook-pro-de-nathan.home>
2020-10-10 19:03:07 +0200

    champ required pour le formulaire + changement du prix au click sur + ou -

--- src/confirmation/confirmation.js
+++ src/confirmation/confirmation.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import './../style.scss';
+let storage = JSON.parse(localStorage.getItem("toto"))
+let panier = storage ? storage : [];
+
+let storagePrice = JSON.parse(localStorage.getItem("price"))
+let price = storagePrice ? storagePrice : [];
+
 
 const urlName = new URL(window.location.href);
 const id = urlName.searchParams.get("name");
 console.log(id);
-const urlEmail = new URL(window.location.href);
-const email = urlEmail.searchParams.get("email");
-console.log(email);
+
 let message = document.getElementById("message");
-message.innerHTML = "merci " + id +  " pour votre commande un email de confirmation vous à été envoyé à " +email;
+message.innerHTML = "merci " + id +  " pour votre commande, le prix total est de : " +price+ "€ votre numero de commande est : " +panier;
+console.log(id);
--- src/panier/panier.js
+++ src/panier/panier.js
@@ -3,6 +3,10 @@
 import _, { add } from 'lodash';
 import './../style.scss';
 
+let storagePrice = JSON.parse(localStorage.getItem("price"))
+let price = storagePrice ? storagePrice : [];
+
+
 let tableau_produit = document.getElementById("tableau-produit");
 console.log("panier.js");
 let storage = JSON.parse(localStorage.getItem("panier"))
@@ -17,25 +21,21 @@ fetch('http://localhost:3000/api/teddies/')
 ;
 
 
-// function price(teddy) {
-//     let total = 0;
-//     total += teddy.price;
-//     let prix_total = document.createElement("p");
-//     tableau_produit.append(prix_total);
-//     prix_total.innerHTML = "prix total: " +total+ "€"
-// }
-
-
 function displayCart(teddies) {
+    let total = 0;
     for (let item of panier) {
         console.log(item.id);
         let teddy = teddies.find(element => element._id === item.id)
         teddy.quantity = item.quantity;
         teddy.selectedColor = item.color;
         displayLine(teddy, "tablePanier");
-        // price(teddy);
+        total += teddy.price;
     };
-    addListeners()
+    let prix_total = document.createElement("p");
+    tableau_produit.append(prix_total);
+    prix_total.innerHTML = "prix total: " +total+ "€"
+    localStorage.setItem("price", JSON.stringify(total));
+    addListeners(teddies);
     acheter();
 }
 
@@ -44,20 +44,23 @@ function displayCart(teddies) {
 function displayLine(teddy, target) {
     let Line = `
         <tr class="border d-flex align-items-center justify-content-around">
-            <td class="pl-2 pr-2">
+            <td class="pl-2 col-md-1 col-2 pr-2">
                 ${teddy.name}
             </td>
-            <td class="pl-2 pr-2">
+            <td class="pl-2 col-md-1 col-2 pr-2">
                 ${teddy.selectedColor}
             </td>
             <td id="tableau-price">
-                ${teddy.price}
+                ${teddy.price}€
             </td>
-            <td class="col-12 col-md-5">
-                <img src="${teddy.imageUrl}" class="w-25">
+            <td id="tableau-price-total_${teddy._id}">
+                ${teddy.price * teddy.quantity}€
+            </td>
+            <td class="col-3 col-md-1 d-flex justify-content-center">
+                <img src="${teddy.imageUrl}" class="img-tableau">
             </td>
             <td>
-                <button id="button-supprimer" data-target-id="${teddy._id}" data-target-color="${teddy.selectedColor}" class="btn btn-danger bouton-supprimer" click="test">
+                <button id="button-supprimer" data-target-id="${teddy._id}" data-target-price="${teddy.price}" data-target-color="${teddy.selectedColor}" class="btn btn-danger bouton-supprimer" click="test">
                     -
                 </button>
             </td>
@@ -65,7 +68,7 @@ function displayLine(teddy, target) {
                 ${teddy.quantity}
             </td>
             <td>
-                <button id="button-ajouter" class="btn btn-primary bouton-ajouter" data-target-id="${teddy._id}" data-target-color="${teddy.selectedColor}">
+                <button id="button-ajouter" class="btn btn-primary bouton-ajouter" data-target-price="${teddy.price}" data-target-id="${teddy._id}" data-target-color="${teddy.selectedColor}">
                     +
                 </button>
             </td>
@@ -73,13 +76,26 @@ function displayLine(teddy, target) {
     document.getElementById(target).innerHTML += Line;
 }
 
+function addPrice(teddies) {
+    let teddy = teddies.map(elt => elt.price);
+    console.log(teddies)
+    let priceUnitaire = teddy.price;
+    let prixCumule = priceUnitaire * item.quantity;
+    price.innerHTML = prixCumule + "€";
+
+    return prixCumule;
+}
+
 function addListeners() {
     console.log(document.querySelectorAll(".bouton-ajouter"));
+    let price = 0;
     [].forEach.call(document.querySelectorAll(".bouton-ajouter"), function(el) {
         console.log(el)
         el.addEventListener('click', function() {
-            console.log("button_ajouter")
-            addItem(el.dataset.targetId, el.dataset.targetColor)
+            console.log(el.dataset.targetPrice)
+            addItem(el.dataset.targetId, el.dataset.targetColor, parseInt(el.dataset.targetPrice))
+            price += parseInt(el.dataset.targetPrice);
+            console.log(price)
         })
     }),
     [].forEach.call(document.querySelectorAll(".bouton-supprimer"), function(el) {
@@ -87,6 +103,8 @@ function addListeners() {
         el.addEventListener('click', function() {
             console.log("button_ajouter")
             deleteItem(el.dataset.targetId, el.dataset.targetColor)
+            price -= parseInt(el.dataset.targetPrice);
+            console.log(price)
         })
     })
 }
@@ -94,22 +112,51 @@ function addListeners() {
 function acheter(){
     document.getElementById("button").addEventListener("click", function(){
         var inputName = document.getElementById("name").value;
+        var inputNom = document.getElementById("nom").value;
+        var inputAdresse = document.getElementById("adresse").value;
+        var inputVille = document.getElementById("ville").value;
         var inputEmail = document.getElementById("email").value;
         // Afficher la valeur
-        console.log(inputName);
+        console.log(inputNom);
         console.log(inputEmail);
-        lien.setAttribute("href", "confirmation.html?name=" +inputName + "&email="  +inputEmail);
+
+        console.log(panier)
+
+        let contact = {
+                firstName: inputName,
+                lastName: inputNom,
+                address: inputAdresse,
+                city: inputVille,
+                email: inputEmail
+            }
+        localStorage.setItem("contact", JSON.stringify(contact));
+        
+        let products = panier.map(elt => elt.id);
+
+        fetch('http://localhost:3000/api/teddies/order', {
+        method: "POST",
+        body: JSON.stringify({contact, products}),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .then(elt => {
+            console.log(elt)
+            localStorage.setItem("toto", JSON.stringify(elt.orderId));
+            // window.location.replace("confirmation.html");
+        }).catch(function(error) {
+            console.log(error);
+        });
     });
 }
 
-function addItem(id, color, teddies) {
-    let item = panier.find(elt => elt.id === id && elt.color === color)
+function addItem(id, color, price) {
+    let item = panier.find(elt => elt.id === id && elt.color === color);
     // console.log(item)
     // console.log(panier, id)
-    // let teddy = teddies.find(element => element > 10);
-    // console.log(teddy);
-    console.log(panier)
+    console.log(panier);
     item.quantity += 1;
+
+    document.getElementById("tableau-price-total_" + id).innerHTML= item.quantity * price;
     document.getElementById("teddy_" +id).innerHTML = item.quantity;
     localStorage.setItem("panier", JSON.stringify(panier));
     // console.log(item);
